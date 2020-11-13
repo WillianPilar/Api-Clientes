b424d74eea31d436ace42dbc5459ea4c019b76f5
Willian Araujo Pilar <willianpilar@brq.com>
2020-11-13 12:47:42 -0300

    CRUD quase completo

--- src/main/java/com/example/Clientes/ApiClientes/Controller/ClientesController.java
+++ src/main/java/com/example/Clientes/ApiClientes/Controller/ClientesController.java
@@ -1,16 +1,42 @@
 package com.example.Clientes.ApiClientes.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Clientes.ApiClientes.Model.Clientes;
+import com.example.Clientes.ApiClientes.Service.ClientesService;
 
 @RestController
 @RequestMapping(value="/api/clientes")
 public class ClientesController {
+		
+	@Autowired
+	public ClientesService clientesService;
 	
 	@GetMapping
-	public String hello() {
-		return "FOOOOI";
+	public List<Clientes> getAllClientes() {
+		return clientesService.getAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Clientes> getClienteById(@PathVariable int id) {
+		return clientesService.getById(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Clientes createCliente(@RequestBody Clientes cliente) {
+		return clientesService.create(cliente);
 	}
 
 }
---
+++ src/main/java/com/example/Clientes/ApiClientes/Service/ClientesService.java
@@ -0,0 +1,35 @@
+package com.example.Clientes.ApiClientes.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Clientes.ApiClientes.Model.Clientes;
+import com.example.Clientes.ApiClientes.Repository.ClientesRepository;
+
+@Service
+public class ClientesService {
+	
+	@Autowired
+	private ClientesRepository clientesRepository;
+	
+	public List<Clientes> getAll() {
+		return clientesRepository.findAll();
+		
+	}
+
+	public Clientes create(Clientes cliente) {
+		
+		return clientesRepository.save(cliente);
+	}
+
+	public Optional<Clientes> getById(int id) {
+
+		return clientesRepository.findById(id);
+	}
+
+	
+
+}
